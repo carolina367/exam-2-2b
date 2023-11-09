c1722faa2d47f22c739afa3fde9ee93760fe20fd
carolina367 <carolina.barboza367@gmail.com>
2023-11-09 22:59:03 +0000

    deadlock

--- burgers_and_fries.cpp
+++ burgers_and_fries.cpp
@@ -12,6 +12,10 @@ const char* type_names[] = {"BURGER", "FRIES"};
 #define pii pair<int, int>
 
 int k;
+sem_t burger_sem;
+sem_t fries_sem;
+mutex mtx;
+int last_order_type = -1;  // -1 indicates no last order
 
 // Do not change
 void process_order() {
@@ -19,31 +23,40 @@ void process_order() {
 }
 
 void place_order(int type) {
-    /**
-     *  Add logic for synchronization before order processing
-     *  Check if already k orders in process; 
-     *     if true -> wait (print waiting)
-     *     otherwise place this order (print order)
-     *  Use type_names[type] to print the order type
-     */
+    mtx.lock();
+    if (type == BURGER) {
+        sem_wait(&burger_sem);  // P() Decrement burger semaphore
+        mtx.unlock();
+        cout << "Order: BURGER" << endl;
+    } else {
+        sem_wait(&fries_sem);  //P()  Decrement fries semaphore
+        mtx.unlock();
+        cout << "Order: FRIES" << endl;
+    }
 
-    process_order();        // Do not remove, simulates preparation
+    process_order();  // Process the order for 2 seconds
 
-    /**
-     *  Add logic for synchronization after order processed
-     *  Allow next order of the same type to proceed if there is any waiting; if not, allow the other type to proceed.
-     */
+    mtx.lock();
+    // Post-processing, increment the semaphore for the next order.
+    // You will need to include logic to determine which type should be incremented based on your waiting logic.
+    if (type == BURGER) {
+        sem_post(&burger_sem); // V()
+    } else {
+        sem_post(&fries_sem); // V()
+    }
+    mtx.unlock();
 }
 
 int main() {
     // Initialize necessary variables, semaphores etc.
-    
+    int t;
+    cin >> k >> t;
+    sem_init(&burger_sem, 0, k);  // Initialize burger semaphore with k
+    sem_init(&fries_sem, 0, k);   // Initialize fries semaphore with k
+
     // Read data: done for you, do not change
     pii incoming[MAX_THREADS];
     int _type, _arrival;
-    int t;
-    cin >> k;
-    cin >> t;
     for (int i = 0; i < t; ++i) {
         cin >> _type >> _arrival;
         incoming[i].first = _type;
@@ -66,5 +79,9 @@ int main() {
         threads[i]->join();
         delete threads[i];
     }
+
+    // Destroy the semaphores
+    sem_destroy(&burger_sem);
+    sem_destroy(&fries_sem);
     return 0;
 }
